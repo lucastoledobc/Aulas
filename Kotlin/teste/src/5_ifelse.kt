0157850058154df4b74451029d70e28cef9337c7
ToledoLBC <lucastoledobm@gmail.com>
2024-01-02 19:51:33 -0300

    kotlin

---
+++ Kotlin/teste/src/5_ifelse.kt
@@ -0,0 +1,14 @@
+fun main () {
+    var num1 = 3
+    var num2 = 3
+    var num3 = 23
+    var num4 = 44
+
+    if (num1 == num2 && num3 < num4) {
+        println("top")
+    } else if (num1 == num2 || num3 > num4) {
+        println("nice")
+    } else {
+        println("ora ora")
+    }
+}
---
+++ Kotlin/teste/src/6_arrays.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main () {
+    var lista = arrayOf("arroz","feijão","ovo",34)
+    println(lista)
+    println(lista[0])
+    println(Arrays.toString(lista))
+    println(lista.size)
+}
