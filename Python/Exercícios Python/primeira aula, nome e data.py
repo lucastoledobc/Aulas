552d7274009c7fb6338306218564b62c0b72f7a8
ToledoLBC <lucastoledobm@gmail.com>
2024-04-15 10:03:54 -0300

    Ajustes nas pastas

--- Python/Exercícios Python/primeira aula, nome e data.py
+++
@@ -1,5 +0,0 @@
-nome=input('Qual é seu nome?')
-dian=input('Que dia você nasceu?')
-mesn=input('Que mês você nasceu?')
-anon=input('E em qual ano?')
-print(nome,', você nasceu em',dian,'/',mesn,'/',anon)
