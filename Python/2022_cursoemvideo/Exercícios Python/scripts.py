c5bec23b63857e939c77bca099f4a048cd2fb1c7
Lumi <lucastoledobm@gmail.com>
2024-07-03 13:48:11 -0300

    ai

--- Python/2022_cursoemvideo/Exercícios Python/scripts.py
+++ Python/2022_cursoemvideo/Exercícios Python/scripts.py
@@ -1,6 +1,6 @@
 print('Vamos somar!')
 n=input('Qual o primeiro número?')
 m=input('Qual o segundo número')
-print('A resposta é:',n+m)
+print('A resposta é:',n + m)
 print('Brincadeira. A resposta é ',int(n)+int(m))
 
